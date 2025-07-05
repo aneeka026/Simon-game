04a9d5fc55d59d6064c6f702c774308fa40a8f84
Aneeka <182370815+aneeka026@users.noreply.github.com>
2025-07-06 01:03:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+let gameSeq=[];
+let userSeq=[];
+
+let started = false; 
+let level = 0;
+
+let h2 = document.querySelector("h2")
+document.addEventListener("keypress", function(){
+    //step 1--> if  any key press game start
+    if(started == false){ 
+        console.log("game is started");
+        started = true;
+
+        levelUp();
+    }
+})
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash")
+    },1000);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    //random btn choose
+    btnFlash();
+}
